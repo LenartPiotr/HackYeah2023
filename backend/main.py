b14cbbb19f62ce9bf53a27103a81ac22d9cfde81
Łukasz Niedzielski <luk20001122@gmail.com>
2023-09-30 14:16:47 +0200

    fix flask not sending responses

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,12 @@
-print("hello world")
+from flask import Flask
+  
+# creates a Flask application
+app = Flask(__name__)
+  
+@app.route("/hello")
+def hello():
+    return "Hello"
+  
+# run the application
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, port=8080)
